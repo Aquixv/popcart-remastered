924d69a548562fb63f7ff92c3eae86311caa6598
unknown <dareakintundealamu@gmail.com>
2026-08-03 15:01:12 +0100

    Battling W resolvers

--- Server/resolvers.ts
+++ Server/resolvers.ts
@@ -1,14 +1,24 @@
-
 import { getProducts, getProductsByCategory } from './controllers/Productcontroller';
 import { registerUser, loginUser } from './controllers/Authcontroller';
+interface CategoryArgs {
+  categoryName: string;
+}
+
+interface LoginArgs {
+  email: string;
+  password: string;
+}
 
 export const resolvers = {
-    Query: {
-        getAllProducts: async () => await getProducts(),
-        getCategory: async (_: any, { categoryName }) => await getProductsByCategory(categoryName),
-    },
-    Mutation: {
-        register: async (_: any, args: any) => await registerUser(args),
-        login: async (_: any, { email, password }) => await loginUser(email, password),
-    }
+  Query: {
+    getAllProducts: async () => await getProducts(),
+
+    getCategory: async (_: any, { categoryName }: CategoryArgs) => 
+      await getProductsByCategory(categoryName),
+  },
+  Mutation: {
+    register: async (_: any, args: any) => await registerUser(args),
+    login: async (_: any, { email, password }: LoginArgs) => 
+      await loginUser(email, password),
+  }
 };
