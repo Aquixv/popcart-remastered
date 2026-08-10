f6ef5c1aeef86ac774303a7a17ff0310916b865f
unknown <dareakintundealamu@gmail.com>
2026-08-10 11:02:11 +0100

    GraphQl installation + uncommitted changes

--- AquiiComms/src/main.tsx
+++ AquiiComms/src/main.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ApolloProvider } from '@apollo/client/react';
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  createHttpLink 
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:1500/graphql', 
+});
+
+const authLink = setContext((_, { headers }) => {
+  const storedData = localStorage.getItem('userInfo'); 
+  let token = "";
+
+  if (storedData) {
+    try {
+      const parsedUser = JSON.parse(storedData);
+      token = parsedUser.token;
+    } catch (error) {
+      console.error("Failed to parse user data from local storage", error);
+    }
+  }
+  
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache() 
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
