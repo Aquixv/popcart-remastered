1a8e9013a7af12884bc155eef4fab8f88a44fae1
unknown <dareakintundealamu@gmail.com>
2026-08-11 18:31:49 +0100

    Queries finished, remains testing though

--- AquiiComms/graphql/queries.ts
+++ AquiiComms/graphql/queries.ts
@@ -75,4 +75,86 @@ query GetSingleProduct($productId: ID!) {
   }
 }
 `
-export const GET_CART = gql ``
+export const GET_CART = gql `
+query GetCart {
+  getCart {
+    _id
+    user {
+      name
+      role
+      avatar
+      _id
+    }
+    items {
+      product {
+        _id
+        title
+        price
+        description
+        category
+        stock
+        thumbnail
+        brand
+        rating
+        numReviews
+        reviews {
+          name
+          comment
+          rating
+        }
+      }
+      quantity
+    }
+  }
+}`
+export const GET_ORDERS = gql `
+query GetMyOrders {
+  getMyOrders {
+    _id
+    user {
+      name
+      avatar
+      _id
+    }
+    orderItems {
+      product {
+        title
+        price
+        reviews {
+          name
+          rating
+          comment
+          user {
+            name
+            avatar
+          }
+        }
+        description
+        category
+        stock
+        thumbnail
+        rating
+        numReviews
+        brand
+      }
+      quantity
+      price
+    }
+    shippingAddress
+    paymentMethod
+    itemsPrice
+    shippingPrice
+    totalPrice
+    isPaid
+    paidAt
+    isDelivered
+    deliveredAt
+  }
+}`
+export const GET_SELLER_REVENUE = gql `
+query GetSellerRevenue {
+  getSellerRevenue {
+    totalRevenue
+    totalItemsSold
+  }
+}`
